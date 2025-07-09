eba07dd98c53b7189512e939bdceb8ac37669644
Radhikesh Mishra <RADHIKES16@GMAIL.COM>
2025-07-09 13:45:09 +0530

    Update AuthApi.js

--- frontend/src/api/AuthApi.js
+++ frontend/src/api/AuthApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:9000' });
+const API = axios.create({ baseURL: 'https://financial-tracker-t3xe.onrender.com' });
 
 export const login = ( formData ) => API.post('/api/login', formData);
 export const register = ( formData ) => API.post('/api/register', formData); 
